a4607f8f60348e0fc2fc5937d68b5d8f970c93f8
oaao <oliver+github@abolish.golf>
2026-03-03 09:52:54 -0700

    (ignore) [04 - react]: ex05 - (ignore) fix indentations

--- 04 - react intro/examples/06 - state/instructor/state/pages/components/TodoList.jsx
+++ 04 - react intro/examples/06 - state/instructor/state/pages/components/TodoList.jsx
@@ -1,3 +1,4 @@
+
 // MUI components
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -9,42 +10,42 @@ import Typography from '@mui/material/Typography';
 
 export default function TodoList() {
 
-    const onTodoTextChange = (event) => {
-      console.log(event.target.value)
-    }
+  const onTodoTextChange = (event) => {
+    console.log(event.target.value)
+  }
 
-    const onAddTodoClick = () => {
-      console.log("clicked!")
-    } 
+  const onAddTodoClick = () => {
+    console.log("clicked!")
+  }
 
 
-    return <Box sx={{ flexGrow: 1 }}>
+  return <Box sx={{ flexGrow: 1 }}>
 
-        <Grid container spacing={2}>
+      <Grid container spacing={2}>
 
-            <Grid item xs={12}>
-              <Typography variant="h2" component="h2">
-                Our Todo List
-              </Typography>
-            </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h2" component="h2">
+              Our Todo List
+            </Typography>
+          </Grid>
 
-            <Grid item xs={10}>
-                <TextField
-                    id="standard-basic"
-                    label="New Todo Item"
-                    variant="standard"
-                    sx={{ width: '100%' }}
-                    onChange={onTodoTextChange}
-                />
-            </Grid>
+          <Grid item xs={10}>
+              <TextField
+                  id="standard-basic"
+                  label="New Todo Item"
+                  variant="standard"
+                  sx={{ width: '100%' }}
+                  onChange={onTodoTextChange}
+              />
+          </Grid>
 
-            <Grid item xs={2}>
-              <Button
-                variant="contained"
-                onClick={onAddTodoClick}
-              >Add Todo</Button>
-            </Grid>
+          <Grid item xs={2}>
+            <Button
+              variant="contained"
+              onClick={onAddTodoClick}
+            >Add Todo</Button>
+          </Grid>
 
-      </Grid>
-    </Box>
+    </Grid>
+  </Box>
 }
