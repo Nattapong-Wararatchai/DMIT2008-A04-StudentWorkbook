b2fe6b624669692d16c8410a01efbb4d25a7ffe7
oaao <oliver+github@abolish.golf>
2026-03-03 09:47:27 -0700

    rev [04 - react]: ex05 - static TodoList with MUI components

--- 04 - react intro/examples/06 - state/instructor/state/pages/components/TodoList.jsx
+++ 04 - react intro/examples/06 - state/instructor/state/pages/components/TodoList.jsx
@@ -1,21 +1,50 @@
+// MUI components
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+
+
 export default function TodoList() {
-    return 
-      <Box sx={{ flexGrow: 1 }}
-        
+
+    const onTodoTextChange = (event) => {
+      console.log(event.target.value)
+    }
+
+    const onAddTodoClick = () => {
+      console.log("clicked!")
+    } 
+
+
+    return <Box sx={{ flexGrow: 1 }}>
+
         <Grid container spacing={2}>
 
-          <Grid item xs={12}>
-            <Typography variant="h2">Our Todo List</Typography>
-          </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h2" component="h2">
+                Our Todo List
+              </Typography>
+            </Grid>
 
-        </Grid>
+            <Grid item xs={10}>
+                <TextField
+                    id="standard-basic"
+                    label="New Todo Item"
+                    variant="standard"
+                    sx={{ width: '100%' }}
+                    onChange={onTodoTextChange}
+                />
+            </Grid>
 
+            <Grid item xs={2}>
+              <Button
+                variant="contained"
+                onClick={onAddTodoClick}
+              >Add Todo</Button>
+            </Grid>
+
+      </Grid>
     </Box>
-
 }
