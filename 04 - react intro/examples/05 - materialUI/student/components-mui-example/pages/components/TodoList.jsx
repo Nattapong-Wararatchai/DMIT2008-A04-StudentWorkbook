d392baf4915c3df3a1eb94171b8d3ebda7cc4e31
Nattapong-Wararatchai <nwararatchai1@nait.ca>
2026-03-04 10:06:37 -0700

    setup mui

---
+++ 04 - react intro/examples/05 - materialUI/student/components-mui-example/pages/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+
+export default function TodoList() {
+    return (
+        <Box sx{ flexGrow: 1}></>
+
+        </Box>
+    )
+}
